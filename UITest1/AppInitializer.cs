075d59ddb3d00d3f2b17364de74aa3766547ca34
Yashwanth1302 <yashwanth1302@gmail.com>
2017-07-27 13:52:56 +0530

    v7

--- UITest1/AppInitializer.cs
+++ UITest1/AppInitializer.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using Xamarin.UITest;
-using Xamarin.UITest.Queries;
+//using Xamarin.UITest;
+//using Xamarin.UITest.Queries;
 
 namespace UITest1
 {
     public class AppInitializer
     {
-        public static IApp StartApp(Platform platform)
-        {
-            if (platform == Platform.Android)
-            {
-                return ConfigureApp
-                     .Android.ApkFile("EretailApp.Android.apk")
-                    .StartApp();
-            }
+        //public static IApp StartApp(Platform platform)
+        //{
+        //    if (platform == Platform.Android)
+        //    {
+        //        return ConfigureApp
+        //             .Android.ApkFile("EretailApp.Android.apk")
+        //            .StartApp();
+        //    }
 
-            return ConfigureApp
-                 .Android.ApkFile("EretailApp.Android.apk")
-                .StartApp();
-        }
+        //    return ConfigureApp
+        //         .Android.ApkFile("EretailApp.Android.apk")
+        //        .StartApp();
+        //}
     }
 }
 
